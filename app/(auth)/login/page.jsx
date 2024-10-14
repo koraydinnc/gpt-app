27e46595ac238078ae28dad566f69a5989a97a60
koraydinnc <koraydincc@icloud.com>
2024-10-14 23:45:07 +0300

    .

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { LoginForm } from '../components/auth/LoginForm'
-
+import {LoginForm} from '../../components/auth/LoginForm'
 const page = () => {
   return (
     <div>
